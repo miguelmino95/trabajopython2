0b644dc019aa268f65ddf761b4c158bb96a43e90
Elvirita <elvira.alvarado@itsae.edu.ec>
2015-02-11 16:42:09 +0100

    no registra

--- proyectofinal/control/appControl.py
+++ proyectofinal/control/appControl.py
@@ -25,34 +25,17 @@ def addActivos():
     nombre=request.form.get('nombre', type=str)
     fcompra=request.form.get('fech_compra', type=str)
     estado=request.form.get('estado', type=str)
-    nfactura=request.form.get('num_factura', type=int)
-    modelo_serie=request.form.get('modelo_serie', type=str)
+    numfactura=request.form.get('num_factura', type=int)
+    modelo=request.form.get('modelo_serie', type=str)
     marca_hardware=request.form.get('marca_hardware', type=str)
     codigo=request.form.get('codigo', type=str)
     descripcion=request.form.get('descripcion', type=str)
     responsable=request.form.get('responsable', type=str)
     area=request.form.get('area', type=str)
 
-    Accionlabcon.Accionlabcon().insertarActivo(nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo,descripcion, responsable, area)
+    Accionlabcon.Accionlabcon().insertarActivo(nombre, fcompra, estado, numfactura, modelo, marca_hardware, codigo, descripcion, responsable, area)
     return redirect(url_for('reportarActivo'))
 
-@control.route("/delActivos")
-def delActivos():
-    
-    nombre=request.form.get('nombre', type=str)
-    fcompra=request.form.get('fech_compra', type=str)
-    estado=request.form.get('estado', type=int)
-    nfactura=request.form.get('num_factura', type=str)
-    modelo_serie=request.form.get('modelo_serie', type=str)
-    marca_hardware=request.form.get('marca_hardware', type=str)
-    codigo=request.form.get('codigo', type=str)
-    descripcion=request.form.get('descripcion', type=str)
-    responsable=request.form.get('responsable', type=str)
-    area=request.form.get('area', type=str)
-
-
-    Accionlabcon.Accionlabcon().EliminarActivo(nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo,descripcion, responsable, area)
-    return redirect(url_for('reportarActivo'))
 
 @control.route("/buscarActivo")
 def buscarActivoDato():
@@ -66,38 +49,4 @@ def buscarActivoAuto():
     objr=Accionlabcon.Accionlabcon().buscarActivoAuto(nombre) 
     print objr
     return objr
-
-'''@control.route("/personas")
-def reportarPersona():
-    objr=Accionlabcon.Accionlabcon().reportarPersona()  
-    return render_template("formRegistroPersonas.html", data=objr)
-
-@control.route("/addPersona", methods=['POST'])
-def addPersona():
-    nombre=request.form.get('nombre', type=str)
-    apaterno=request.form.get('apaterno', type=str)
-    amaterno=request.form.get('amaterno', type=str)
-    cedula=request.form.get('cedula', type=str)
-    telefono=request.form.get('telefono', type=str)
-    email=request.form.get('email', type=str)
-    fnacimiento=request.form.get('fnacimiento', type=str)    
-    edad=request.form.get('edad', type=str)    
-    estado=request.form.get('estado', type=int)
-
-
-    Accionlabcon.Accionlabcon().insertarPersona(nombre, apaterno, amaterno, cedula, telefono, email, fnacimiento, edad, estado)
-    return redirect(url_for('reportarPersona'))
-
-@control.route("/buscarPersona")
-def buscarPersonaDato():
-    nombre=str(request.args.get('Pnombre'))
-    objr=Accionlabcon.Accionlabcon().buscarPersonaDato(nombre) 
-    return render_template("formRegistroPersonas.html", data=objr)
-
-@control.route("/buscarperauto")
-def buscarPersonaAuto():
-    nombre=str(request.args.get('term'))
-    objr=Accionlabcon.Accionlabcon().buscarPersonaAuto(nombre) 
-    print objr
-    return objr'''
 
--- proyectofinal/ec/edu/itsae/labcon/Accionlabcon.py
+++ proyectofinal/ec/edu/itsae/labcon/Accionlabcon.py
@@ -32,24 +32,16 @@ class Accionlabcon(DBconec.DBcon):
         reporte=con.fetchall()
         return reporte
     
-    def insertarActivo(self, nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area):
+    
+    def insertarActivo(self,nombre, fcompra, estado, numfactura, modelo, marca_hardware, codigo, descripcion, responsable, area):
         con=self.conexion().connect()
-        sql=""" insert into activos (nombre, fech_compra, estado, num_factura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area) 
-        values( '%s', '%s', '%s', %i, '%s', '%s', '%s', '%s', '%s', '%s' ) """ %(nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area)
-        print sql
+        sql=""" insert into activos(nombre, fech_compra, estado, num_factura, 
+                        modelo_serie, marca_hardware, codigo, descripcion, responsable, area)
+                         values('%s','%s','%s', %i ,'%s','%s','%s','%s','%s','%s')  """ % (nombre, fcompra, estado, numfactura, modelo, marca_hardware, codigo, descripcion, responsable, area)
+        return sql
         with con:
             cursor=con.cursor()
-            cursor.execute(sql)  
-            
-            
-    def EliminarActivo(self, nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area):
-        con=self.conexion().connect()
-        sql=""" delete from activos where (nombre, fech_compra, estado, num_factura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area) 
-        values('%s', '%s', '%s' , %i, '%s', '%s', '%s' , '%s', '%s' '%s') """ % (nombre, fcompra, estado, nfactura, modelo_serie, marca_hardware, codigo, descripcion, responsable, area)
-        print sql
-        with con:
-            cursor=con.cursor()
-            cursor.execute(sql)
+            cursor.execute(sql)     
             
     def buscarActivoDato(self, datobuscado):
         con=self.conexion().connect().cursor() 
@@ -67,36 +59,6 @@ class Accionlabcon(DBconec.DBcon):
             lista.append(dict(zip(columna, row)))
         return json.dumps(lista, indent=2)
     
-    '''def reportarPersona(self):
-        con=self.conexion().connect().cursor()
-        con.execute("select * from persona")
-        reporte=con.fetchall()
-        return reporte
-    
-    def insertarPersona(self, nombre, apaterno, amaterno, cedula, telefono, email, fnacimiento, edad, estado):
-        con=self.conexion().connect()
-        sql=""" insert into persona(nombre, apell_paterno, apell_materno, cedula, telefono, email, fech_nacimiento, edad, estado) 
-        values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %i) """ % (nombre, apaterno, amaterno, cedula, telefono, email, fnacimiento, edad, estado)
-        print sql
-        with con:
-            cursor=con.cursor()
-            cursor.execute(sql)
-            
-    def buscarPersonaDato(self, datobuscado):
-        con=self.conexion().connect().cursor() 
-        con.execute(""" select * from persona where upper(CONCAT(nombre,' ', apell_paterno,' ', apell_materno)) like upper('%s') """ %("%"+datobuscado+"%"))
-        reporte=con.fetchall()
-        return reporte
-    
-    def buscarPersonaAuto(self, datobuscado):
-        con=self.conexion().connect().cursor()
-        con.execute(""" select CONCAT(nombre, ' ', apell_paterno, ' ', apell_materno) as value, idpersona as id from persona where upper (CONCAT(nombre, ' ', apell_paterno, ' ', apell_materno)) like upper('%s')""" % ("%"+datobuscado+"%") )
-        reporte=con.fetchall()
-        columna=('value', 'id')
-        lista=[]
-        for row in reporte:
-            lista.append(dict(zip(columna, row)))
-        return json.dumps(lista, indent=2)'''
     
     
             
